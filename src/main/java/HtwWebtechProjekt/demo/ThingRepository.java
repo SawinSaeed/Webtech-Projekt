b335c5c5d0257e67c05ec499548ce1d6d99730c7
Sawin Saeed <Sawin.Saeed@Student.Htw.Berlin.de>
2023-11-06 15:25:30 +0100

    M2

---
+++ src/main/java/HtwWebtechProjekt/demo/ThingRepository.java
@@ -0,0 +1,7 @@
+package HtwWebtechProjekt.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ThingRepository extends CrudRepository<Thing, Long> { }
